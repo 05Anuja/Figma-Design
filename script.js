d1d116168f3dd1ab21fde1fafe83ca734701acdd
Anuja-Bhimraj-Pawar <anuja13122004@gmail.com>
2025-05-24 02:03:21 +0530

    Changes

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const hamburger = document.getElementById("hamburger");
-  const sections = document.getElementById("sections");
-  const contactBtn = document.getElementById("contact-btn");
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
 
-  hamburger.addEventListener("click", () => {
-    sections.classList.toggle("show");
-    contactBtn.classList.toggle("show");
-  });
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
 });
